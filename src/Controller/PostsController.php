2985a693560721487ce845fef26da5114fed7a08
caiiiok254 <kiskamiska2000@gmail.com>
2020-04-03 15:33:54 +0700

    Now all posts from DB parsed and showing in template

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,18 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
+    /** @var PostRepository $postRepository */
+
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
     /**
      * @Route("/posts", name="posts")
      */
+
+    public function posts()
+    {
+        $posts = $this->postRepository->findAll();
+
+        return $this->render('posts/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
     public function index()
     {
+        $posts = [
+            'post_1' => [
+                'title' => 'Заголовок первого поста',
+                'body' => 'Тело первого поста'
+            ],
+            'post_2' => [
+                'title' => 'Заголовок второго поста',
+                'body' => 'Тело второго поста'
+            ]
+        ];
+
         return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
+            'posts' => $posts,
         ]);
     }
 }
